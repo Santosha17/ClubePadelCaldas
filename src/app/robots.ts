45886acb495fabd186a33bf847a0aeb86965c793
JoãoSantos497 <210100497@esg.ipsantarem.pt>
2026-02-16 15:43:38 +0000

    updates

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,13 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-    // Substitui pelo teu domínio real quando tiveres um (ex: https://clubepadelcaldas.pt)
     const baseUrl = 'https://clubepadeldascaldas.vercel.app';
 
     return {
         rules: {
-            userAgent: '*',     // Aplica-se a todos os robôs (Google, Bing, etc.)
-            allow: '/',         // Permite indexar tudo
-            disallow: '/private/', // (Opcional) Bloqueia pastas privadas, se tiveres
+            userAgent: '*',
+            allow: '/',
+            disallow: '/private/',
         },
         sitemap: `${baseUrl}/sitemap.xml`,
     };
